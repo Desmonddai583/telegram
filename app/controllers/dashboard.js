eebbcfa1a9e8370f0370938a9937a76616d7cca7
Desmond dai <desmonddai583@gmail.com>
2014-09-04 14:02:04 +0800

    add publish action

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -6,4 +6,21 @@ export default Ember.ArrayController.extend({
   remainingWords: function() {
     return 140 - this.get('body').length;
   }.property('body'),
+
+  actions: {
+    publish: function() {
+      var self = this;
+      var body = this.get('body');
+
+      var post = this.store.createRecord('post', {
+        body: body,
+        date: new Date(),
+        author: this.get('model').get('firstObject.author'),
+      });
+
+      post.save();
+
+      this.set('body', '');
+    },
+  },
 });
