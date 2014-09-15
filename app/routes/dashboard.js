5ad63e879a7dd8aa5ccdeef4f0f6f6300c1afbcd
Desmond dai <desmonddai583@gmail.com>
2014-09-15 12:10:22 +0800

    fix session issue

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel: function() {
+    if(!this.get('session.isAuthenticated'))
+      this.transitionTo('auth');
+  },
+
   model: function() {
   	return this.store.find('post');
   },
