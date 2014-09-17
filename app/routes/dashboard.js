9102d3be6c87546bc65de15d4c852d18069bb643
Desmond dai <desmonddai583@gmail.com>
2014-09-17 12:03:51 +0800

    add curly brace

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel: function() {
-    if(!this.get('session.isAuthenticated'))
+    if(!this.get('session.isAuthenticated')) {
       this.transitionTo('auth');
+    }
   },
 
   model: function() {
