4d501085d7fb86888d6d290cec506ba1faf913cd
Desmond dai <desmonddai583@gmail.com>
2014-10-15 18:03:40 +0800

    reset password WIP

--- app/controllers/auth/forgotpass.js
+++ app/controllers/auth/forgotpass.js
@@ -1,9 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  email: null,
+
   actions: {
     reset: function() {
-      this.transitionToRoute('auth.forgotpass_confirm');
+      var self = this;
+      var email = self.get('email');
+      $.ajax({
+        url: '/api/resetPassword/',
+        type: 'POST',
+        dataType: 'json',
+        data: {'email': email},
+        success: function() {    
+          this.transitionToRoute('auth.forgotpass_confirm');
+        },
+      });
     }
   }
 });
