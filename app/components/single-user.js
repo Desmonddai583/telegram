5a8fbf9518276792592f963553048b78bdb81ee1
Desmond dai <desmonddai583@gmail.com>
2014-10-24 11:01:22 +0800

    fix the issues feedback from vlad

--- app/components/single-user.js
+++ app/components/single-user.js
@@ -3,5 +3,14 @@ import Ember from "ember";
 export default Ember.Component.extend({
   isSelf: function() {
     return this.get('user') === this.get('session.user');
-  }.property('user', 'session.user')
+  }.property('user', 'session.user'),
+
+  actions: {
+    unfollow: function(user) {
+      this.sendAction('unfollow', user);
+    },
+    follow: function(user) {
+      this.sendAction('follow', user);
+    }
+  }
 });
