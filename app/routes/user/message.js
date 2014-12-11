6b4c06a15a8636cc726772bf47c2d36632fbf096
Desmond dai <desmonddai583@gmail.com>
2014-12-11 16:48:56 +0800

    user can not see his/her own chat tab

--- app/routes/user/message.js
+++ app/routes/user/message.js
@@ -1,6 +1,15 @@
 import Ember from 'ember';
+import Notify from 'ember-notify';
+Notify.useBootstrap();
  
 export default Ember.Route.extend({
+  beforeModel: function() {
+    if(this.modelFor('user').get("id") === this.get("session.user.id")) {
+      Notify.alert("User do not have own chat tab!");
+      this.transitionTo('auth');
+    }
+  },
+
   model: function() {
     return this.store.find('message', {user: this.modelFor('user').get("id")});
   }
